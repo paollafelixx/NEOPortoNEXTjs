e8603128409b16b4ec8ac829a9d488c8a20e6886
Diogo Moreira <104456651+diogomoreirax@users.noreply.github.com>
2023-11-10 23:32:05 -0300

    ddd

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,20 @@
 // pages/about.js
 import Link from 'next/link';
+import styles from '../styles/styles.css';
 
 const About = () => {
   return (
     <div>
-      <h2>Página Sobre Nós</h2>
-      <p>Esta é a página sobre nós.</p>
-
-      {/* Link de volta para a página inicial */}
-      <Link href="/">Home</Link>
+      <header className={styles.header}>
+        <h2>Página Sobre Nós</h2>
+      </header>
+      <main className={styles.main}>
+        <p>Esta é a página sobre nós.</p>
+        <Link href="/">Home</Link>
+      </main>
+      <footer className={styles.footer}>
+        <p>Rodapé - © 2023</p>
+      </footer>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 // pages/index.js
 import Link from 'next/link';
+import styles from '../styles/styles.css';
 
 const Home = () => {
   return (
     <div>
-      <h2>Home Page</h2>
-      <p>Bem-vindo à página inicial!</p>
-      
-      {/* Link para a página "About" */}
-      <Link href="/about">Sobre nós</Link>
+      <header className={styles.header}>
+        <h2>Home Page</h2>
+      </header>
+      <main className={styles.main}>
+        <p>Bem-vindo à página inicial!</p>
+        <Link href="/about">Sobre nós</Link>
+      </main>
+      <footer className={styles.footer}>
+        <p>Rodapé - © 2023</p>
+      </footer>
     </div>
   );
 };
