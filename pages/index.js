00127a8eb050a551e249869ddac02f0744846b44
Diogo Moreira <104456651+diogomoreirax@users.noreply.github.com>
2023-11-11 19:25:14 -0300

    isso

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,44 @@
 // pages/index.js
-import Link from 'next/link';
+import Head from 'next/head';
+import '../styles/styles.css';  // Importe o arquivo styles.css diretamente
 
-const Home = () => {
-  return (
-    <div>
-      <h2>Home Page</h2>
-      <p>Bem-vindo à página inicial!</p>
+const Home = () => (
+  <div>
+    <Head>
+      {/* ... */}
+    </Head>
+
+    <main className="chat-container">
       
-      {/* Link para a página "About" */}
-      <Link href="/about">Sobre nós</Link>
-    </div>
-  );
-};
+      <div className="message-container">
+
+        <div className="message sent">
+          <p>Olá</p>
+        </div>
+
+        <div className="message received">
+          <p>Você entrou em contato com o PortoHelp, 
+              o mais novo serviço de atendimento ao cliente
+              da Porto Seguro.
+              Vou precisar do CPF do propietário do veículo!</p>
+        </div>
+
+      </div>
+      
+      <div className="input-container">
+        <input
+          type="text"
+          className="input"
+          placeholder="Digite sua mensagem..."
+        />
+        <button className="button">Enviar</button>
+      </div>
+    </main>
+
+    <footer>
+
+    </footer>
+  </div>
+);
 
 export default Home;
